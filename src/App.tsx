07f6d5bd07e02d167ccd326113360bfa456c9d9f
Nikita <nickpelmog2003@gmail.com>
2023-10-08 22:36:55 +0300

    FEAT: add base markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      Default Page
-    </>
-  )
+    <div className="main">
+      <div className="container">
+        <h1 className="mainTitle">Конвертер валют</h1>
+        <h3 className="mainSubtitle">По курсу НБ РБ</h3>
+        <div className="inputsContainer">
+          <div className="inputContainer">
+            <p className="currencyTitle">USD</p>
+            <input />
+          </div>
+          <div className="inputContainer">
+            <p className="currencyTitle">EUR</p>
+            <input />
+          </div>
+          <div className="inputContainer">
+            <p className="currencyTitle">BYN</p>
+            <input />
+          </div>
+          <div className="inputContainer">
+            <p className="currencyTitle">RUB</p>
+            <input />
+          </div>
+          <button>Добавить валюту</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
