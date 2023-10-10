92fd060134551327ba113450d895a3b4045b89a4
Nikita <nickpelmog2003@gmail.com>
2023-10-10 19:51:04 +0300

    INT-1/FEAT: add api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import "./App.css";
 import Currency from "./components/Currency/Currency";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 function App() {
   const [currency, setCurrency] = useState([
-    { title: "USD", id: 0 },
-    { title: "EUR", id: 1 },
-    { title: "BYN", id: 2 },
-    { title: "RUB", id: 3 },
+    { code: "USD", id: 0 },
+    { code: "EUR", id: 1 },
+    { code: "BYN", id: 2 },
+    { code: "RUB", id: 3 },
   ]);
+  let requestHandler = "";
+  for (let i = 0; i < currency.length; i++) {
+    requestHandler = requestHandler + currency[i].code + "%2C";
+  }
+  requestHandler = requestHandler.substring(0, requestHandler.length - 3);
+
+  useEffect(() => {
+    async function getCurrencies() {
+      try {
+        const response = await axios.post("http://localhost:5000/currency", {
+          requestHandler,
+        });
+        const res = Object.values(response?.data?.data);
+        console.log(res);
+        setCurrency(res);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getCurrencies();
+  }, []);
+
   const addCurrencyHandler = (text) => {
     const newCurrency = {
       title: text,
@@ -30,7 +53,6 @@ function App() {
             <Currency
               key={currency.id}
               currency={currency}
-              currName={currency.title}
               deleteCurrency={deleteCurrencyHandler}
             />
           ))}
--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,10 +1,10 @@
 import styles from "./Currency.module.scss";
 
-const Currency = ({ currName, deleteCurrency, currency }) => {
+const Currency = ({ deleteCurrency, currency }) => {
   return (
     <div className={styles.inputContainer}>
-      <p className={styles.currencyTitle}>{currName}</p>
-      <input />
+      <p className={styles.currencyTitle}>{currency.code}</p>
+      <input value={currency.value} />
       <button
         className={styles.deleteButton}
         onClick={() => deleteCurrency(currency.id)}
